e5a8dd39bd7e3c28f061a32110561e94dfca674c
Bartłomiej Gwóźdź <bartek.gw@op.pl>
2026-06-09 20:04:29 +0200

    użycie systemu połączenia, który powinien zadziałać na aplikacji

--- game/constants.js
+++ game/constants.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:5000";
+export const API_URL =
+  process.env.EXPO_PUBLIC_API_URL ?? "http://localhost:5000";
 
 // ─────────────────────────────────────────────────────────────
 //  DIFFICULTIES
